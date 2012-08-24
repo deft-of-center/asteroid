7034cbc975c0a31d2787be511951f1f6a0537c07
James Gill <jamesagill@gmail.com>
2012-08-23 23:00:53 -0700

    Store the edit string in the backing db.

--- asteroid.js
+++ asteroid.js
@@ -1,11 +1,45 @@
-var editString = "";
+function dump(arr,level) {
+	var dumped_text = "";
+	if(!level) level = 0;
+	
+	//The padding given at the beginning of the line.
+	var level_padding = "";
+	for(var j=0;j<level+1;j++) level_padding += "    ";
+	
+	if(typeof(arr) == 'object') { //Array/Hashes/Objects 
+		for(var item in arr) {
+			var value = arr[item];
+			
+			if(typeof(value) == 'object') { //If it is an array,
+				dumped_text += level_padding + "'" + item + "' ...\n";
+				dumped_text += dump(value,level+1);
+			} else {
+				dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+			}
+		}
+	} else { //Stings/Chars/Numbers etc.
+		dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+	}
+	return dumped_text;
+}
+
+
+
+var Docs = new Meteor.Collection("documents", null);
+//Temporary code.  Eventually each REST endpoint will be an Id.
+var docId = 1;
+Docs.insert({id:docId, title:"My Doc", body:"A Test"});
+
 
 if (Meteor.is_client) {
+
+  doc = Docs.findOne({id:docId});
+  Template.hello.title = doc.title;
   Template.hello.greeting = function () {
     return "Welcome to asteroid.";
   };
 
-  Template.hello.editString = editString;
+  Template.hello.editString = doc.body;
 
   Template.hello.events = {
     'click input' : function () {
@@ -16,7 +50,8 @@ if (Meteor.is_client) {
   };
 
   $('#editArea').keyup(function() {
-      editString = Template.hello.editString.value;
+      doc.body = Template.hello.editString.value;
+      Docs.update({id:doc.id}, {body:doc.body}); 
   });
 }
 
