ac99deea62d0b2ada595b487f456e400d3c35ece
James Gill <jamesagill@gmail.com>
2012-08-25 18:42:40 -0700

    Now with multiple (not-working) editors.
    They seem to get rerendered, destroying them.

--- asteroid.js
+++ asteroid.js
@@ -17,9 +17,11 @@ if (Meteor.is_client) {
   //Watch for changes in selected_doc
   Meteor.autosubscribe(function () {
       if (Session.get('selected_doc')) {
-          console.log('Autosubscribe: ' + Session.get('selected_doc') );
+          var docId = Session.get('selected_doc'); 
+          console.log('Autosubscribe: ' + docId );
           setTimeout(function() {
-              var Editor = ace.edit("editor");
+              console.log("Changing editor" + docId + " into editable field.");
+              var Editor = ace.edit("editor"+docId);
               Editor.on("change", function(e) {
                   console.log("Received change", e);
                   var change = {timestamp: new Date().getTime(), user: null, uuid: guidGenerator(),
@@ -52,7 +54,7 @@ if (Meteor.is_client) {
 
       if (changes && Session.get('selected_doc')) {
           setTimeout(function() {
-              var Editor = ace.edit("editor");
+              var Editor = ace.edit("editor"+Session.get('selected_doc'));
               var EditSession = Editor.getSession();
               var Document = EditSession.getDocument();
               console.log('Found Document ', Document);
@@ -72,57 +74,22 @@ if (Meteor.is_client) {
   };
 
 
-  Template.docs.documents = Docs.find();
+  Template.container.documents = Docs.find();
   
   Template.doc.events = {
     'click': function () {
-      console.log("Selecting doc " + this._id);
+      console.log("Selecting doc ", this._id);
       Session.set("selected_doc", this._id);
     }
   };
 
-  Template.edit.docId = function () {
-      return Session.get("selected_doc");
+  Template.editor.display = function () {
+      console.log("Checking for visiblity", Session.get("selected_doc"), this._id);
+      var isVisible = Session.equals("selected_doc", this._id);
+      return isVisible ? "block" : "none";
   };
 
-  Template.edit.title = function () {
-      var aDoc = Docs.findOne(Session.get("selected_doc"));
-      return aDoc && aDoc.title;
-  };
-
-  Template.edit.editString = function () {
-    var aDoc = Docs.findOne(Session.get("selected_doc"));
-    return aDoc && aDoc.body;
-  };
-
-  Template.edit.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      var aDoc = Docs.findOne(Session.get("selected_doc"));
-      if (aDoc) {
-      aDoc.title += "!";
-      console.log("You pressed the button. title is now " + aDoc.title);
-      Docs.update(Session.get("selected_doc"), {$set: {title: aDoc.title}});
-      //console.log("Database title is: " +  Docs.findOne(Session.get("selected_doc")).title );
-      //console.log("Database doc is: " + dump(Docs.findOne(Session.get("selected_doc"))) );
-      } else {
-          console.log("Unable for find document for id " + Sesssion.get("selected_doc") + " in DB of size " + Docs.find().count());
-      }
-    }
-  };
-
-/*
-  $(function() {
-      $(document).keyup(function() {
-          var newBody = $('#editArea').val();
-          console.log("Setting doc body to :" + newBody);
-          Docs.update(Session.get("selected_doc"), {$set: {body:newBody}}); 
-          console.log("Database body is: " +  Docs.findOne(Session.get("selected_doc")).body );
-      });
-  });
-*/
 }
-
 
 if (Meteor.is_server) {
   Meteor.startup(function () {
