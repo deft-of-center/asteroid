75f8a293c4f428204535581c93fc89711dd25b30
James Gill <jamesagill@gmail.com>
2012-08-27 22:46:48 -0700

    Starting ChangeGlommer, but it's not functional yet.

---
+++ ChangeGlommer.js
@@ -0,0 +1,97 @@
+
+console.log("Loading ChangeGlommer");
+//Default chunking period of changes, in ms
+var GLOMMER_DEFAULT_REASONABLE_PAUSE = 400;
+
+var INSERT = 'insertText';
+
+
+ChangeGlommer = function(reasonable_pause_ms, callbacks) { 
+    this.reasonable_pause_ms = reasonable_pause_ms || GLOMMER_DEFAULT_REASONABLE_PAUSE;
+    this.callbacks = callbacks || [];
+    this.pending_queue = [];
+    this.glommed_queue = [];
+    this.timeout_id = null;
+};
+
+ChangeGlommer.prototype = {
+    push: function(change) {
+        this.pending_queue.push(change);
+        if (this.timeout_id) clearTimeout(this.timeout_id);
+        var self = this;
+        this.timeout_id = setTimeout(function() {
+            console.log("Calling glom.");
+           self.glom(); 
+        }, GLOMMER_DEFAULT_REASONABLE_PAUSE);
+    },
+
+    shift: function() {
+        return this.glommed_queue.shift();
+    },
+
+    //Combine any number of changes
+    glom: function() {
+        var change = null;
+        while (this.pending_queue.length > 0) {
+            if (!change) {
+                change = this.pending_queue.shift();
+            } else {
+                //Combine next change with previous, "glommed" changes
+                var next_change = this.pending_queue.shift();
+                if ( this.should_combine(change, next_change) ) {
+
+                    change = this.combine_changes(change, next_change);
+                } else {
+                    this.glommed_queue.push(change);
+                    change = next_change;
+                }
+            }
+        }
+        if (change) this.glommed_queue.push(change);
+        console.log("Finished glomming with glommed_queue: " + JSON.stringify(this.glommed_queue));
+        if (this.glommed_queue.length > 0) {
+            for (var i in this.callbacks) {
+                console.log("Calling callback: " + this.callbacks[i]);
+                this.callbacks[i]();
+            }
+        }
+    },
+
+    should_combine: function(change1, change2) {
+        //console.log("Checking should_combined for change1: " + JSON.stringify(change1) +
+                //" and change2: " + JSON.stringify(change2));
+        if (!change2) {
+            console.log("No change2, returning false.");
+            return false;
+        }
+        if ( change1.user != change2.user ) {
+            console.log("Users are not equal, returning false.");
+            return false;
+        }
+        if ( (change2.timestamp - change1.timestamp) < this.reasonable_pause_ms ) {
+            console.log("Timestamps are too far apart, returning false.");
+            return false;
+        }
+
+        console.log("Returning true from should_combine.");
+        return true;
+    },
+
+    combine_changes: function(change1, change2) {
+        //TODO: Stub out Meteor.uuid().
+        //var new_change = {uuid:Meteor.uuid(), user:change1.user, timestamp:change1.timestamp, data:{}};
+        var new_change = {uuid:Math.random() + "", user:change1.user, timestamp:change1.timestamp, data:{}};
+        if (change1.data.action == INSERT && change2.data.action == INSERT) {
+            console.log("Combining change with action " + change1.data.action + " and action2: " + change2.data.action);
+            new_change.data.action = INSERT;
+            new_change.data.text = change1.data.text + change2.data.text;
+            new_change.data.range = {start:change1.data.range.start, end:change2.data.range.end};
+        }
+        console.log("Returning combined change: " + JSON.stringify(new_change));
+        return new_change;
+    }
+
+          
+};
+
+
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,139 @@
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+var assert = require("assert");
+var Glommer = require('./../ChangeGlommer.js');
+
+console.log("Starting tests.");
+//paste should be an atomic
+//text inserts in difff positions shoudl be difff
+//events from api are not glommed
+//Addr + ess\nSt + ate -> address
+
+
+describe('ChangeGlommer', function() {
+
+    var c = {"timestamp":1346109469573,"user":null,"uuid":"77660c97-1c95-4e9e-89d0-374baece2440","data":{"action":"insertText","range":{"start":{"row":2,"column":0},"end":{"row":2,"column":1}},"text":"c"}};
+    var a = {"timestamp":1346109469790,"user":null,"uuid":"c61d1d63-6308-461a-8b55-d0b592a2cab2","data":{"action":"insertText","range":{"start":{"row":2,"column":1},"end":{"row":2,"column":2}},"text":"a"}};
+    var t = {"timestamp":1346109470109,"user":null,"uuid":"2bdabf21-0b4e-4b16-8e87-5db58e51149a","data":{"action":"insertText","range":{"start":{"row":2,"column":2},"end":{"row":2,"column":3}},"text":"t"}};
+    var BS1 = {"timestamp":1346109469573,"user":null,"uuid":"788f415b-3d6d-47a7-8b9d-ac45bbefc621","data":{"action":"removeText","range":{"start":{"row":1,"column":1},"end":{"row":1,"column":2}},"text":"c"}};
+    var BS2 = {"timestamp":1346109469966,"user":null,"uuid":"788f415b-3d6d-47a7-8b9d-ac45bbefc621","data":{"action":"removeText","range":{"start":{"row":1,"column":1},"end":{"row":1,"column":2}},"text":"a"}};
+    var BS3 = {"timestamp":1346109470066,"user":null,"uuid":"888fc7ca-0430-49a8-bc2c-993389e05d13","data":{"action":"removeText","range":{"start":{"row":3,"column":0},"end":{"row":3,"column":1}},"text":"x"}};
+    var late_a = {"timestamp":1346109470790,"user":null,"uuid":"c61d1d63-6308-461a-8b55-d0b592a2cab2","data":{"action":"insertText","range":{"start":{"row":2,"column":1},"end":{"row":2,"column":2}},"text":"a"}};
+
+    describe('Constructor', function() {
+        it('should load', function() {
+            assert(ChangeGlommer);
+            var glommer = new ChangeGlommer();
+            assert(glommer);
+        });
+    });
+
+    describe('Glommer', function() {
+        it ('should glom c and a to single change', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                assert(glommer.shift());
+                assert( !glommer.shift());
+                console.log("Calling done.");
+                done();
+            }]);
+            glommer.push(c);
+            glommer.push(a);
+        });
+
+        it ('should glom c and a to ca', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                var change = glommer.shift();
+                console.log("Found glommed_change: " + JSON.stringify(change));
+                assert.equal(change.data.action, 'insertText');
+                assert.equal(change.data.text, 'ca');
+                assert.equal(change.data.range.start, c.data.range.start);
+                assert.equal(change.data.range.end, a.data.range.end);
+                console.log("Calling done.");
+                done();
+            }]);
+            glommer.push(c);
+            glommer.push(a);
+        });
+
+        it ('should glom c + a + t to cat', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                var change = glommer.shift();
+                assert.equal(change.data.action, 'insertText');
+                assert.equal(change.data.text, 'cat');
+                assert.equal(change.data.range.start, c.data.range.start);
+                assert.equal(change.data.range.end, t.data.range.end);
+                done();
+            }]);
+            glommer.push(c);
+            glommer.push(a);
+            glommer.push(t);
+        });
+
+        it ('should glom c + a + ^h to c', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                var change = glommer.shift();
+                assert.equal(change.data.action, 'insertText');
+                assert.equal(change.data.text, 'c');
+                assert.equal(change.data.range.start, c.data.range.start);
+                assert.equal(change.data.range.end, c.data.range.end);
+                done();
+            }]);
+            glommer.push(c);
+            glommer.push(a);
+            glommer.push(BS2);
+        });
+
+        it ('should glom c + ^h to null', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                var change = glommer.shift();
+                console.log("************Null test found change " + JSON.stringify(change));
+                assert.not.exists(change);
+                done();
+            }]);
+            glommer.push(c);
+            glommer.push(BS1);
+        });
+
+        it ('should glom ^h + ^h + ^h to ^h^h^h', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                var change = glommer.shift();
+                assert.equal(change.data.action, 'removeText');
+                assert.equal(change.data.text, 'cax');
+                assert.equal(change.data.range.start, BS3.data.range.start);
+                assert.equal(change.data.range.end, BS1.data.range.end);
+                done();
+            }]);
+            glommer.push(BS1);
+            glommer.push(BS2);
+            glommer.push(BS3);
+        });
+
+        it ('should glom c + ^h + ^h to ^h', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                var change = glommer.shift();
+                assert.equal(change.data.action, 'removeText');
+                assert.equal(change.data.text, 'a');
+                assert.equal(change.data.range.start, BS2.data.range.start);
+                assert.equal(change.data.range.end, BS2.data.range.end);
+                done();
+            }]);
+            glommer.push(c);
+            glommer.push(BS1);
+            glommer.push(BS2);
+        });
+
+        it ('should not glom c and a if separated by 1s', function(done) {
+            var glommer = new ChangeGlommer(200, [function() {
+                var change_1 = glommer.shift();
+                var change_2 = glommer.shift();
+                var change_3 = glommer.shift();
+                assert.equal(change_1, c);
+                assert.equal(change_2, a);
+                assert.equal(!change_3);
+                done();
+            }]);
+            glommer.push(c);
+            glommer.push(late_a);
+        });
+    });
+
+});
+
